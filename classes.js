005d736f9b520b47729dc9b38fe5d39d47cd4393
dd-tech-nz <ddumolo@hotmail.com>
2020-02-20 12:21:10 +1300

    complete Coloured Dots in DOM

--- classes.js
+++ classes.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+ one()
+ two()
+ three()
+ makeVisible()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -18,8 +21,27 @@ function one () {
   one.classList.add('blue')
 }
 
+
+
+
 // CREATE FUNCTION two HERE
 
+function two () {
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
 
+function three() {
+  var three = document.getElementById('three')
+
+  three.classList.add('orange')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var invisible = document.getElementsByClassName('invisible');
+  for (var i = 0; i < invisible.length; i++ ) {
+      invisible[i].classList.add('visible');
+}
+ }
